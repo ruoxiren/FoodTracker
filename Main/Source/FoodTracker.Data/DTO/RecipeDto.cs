d9a806ec9961ae2d02761fa7a7e11cea25755f1f
Ruoxi Ren <ruoxiren_1007@163.com>
2023-12-28 19:40:39 -0500

    rename file name

---
+++ Main/Source/FoodTracker.Data/DTO/RecipeDto.cs
@@ -0,0 +1,15 @@
+﻿using FoodTracker.Data.Enum;
+
+namespace FoodTracker.Data.DTO
+{
+    public class RecipeDto
+    {
+        public required string Name { get; set; }
+
+        public required ServingSizeOption ServingSize { get; set; }
+
+        public required decimal Calories { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
